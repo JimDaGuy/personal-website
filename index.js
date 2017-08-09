9d81e2b7db1a77bc8885ae53657f201eaba9fab2
James DiGrazia <isoawc@gmail.com>
2017-08-09 19:15:07 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+/* global $ */ 
+
+//Slick slider
+$(document).ready(function() {
+  $('.socialMedia').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    arrows: true,
+    draggable: true
+  });
+});
+
